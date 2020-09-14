e07b72eb4b808490582f42f1b74a77cf507fe841
lunnerwolf <abhigaur97@gmail.com>
2020-09-15 00:06:38 +0530

    Updates

---
+++ precache-manifest.a0ce84cc5be8f55f40c592c0b4281ad6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc668dd80e71314fc7da0c08908a3869",
+    "url": "/React-demo_website/index.html"
+  },
+  {
+    "revision": "1494e039d68cad1e1dfa",
+    "url": "/React-demo_website/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "bf7c7c76a98f3ce1bd8b",
+    "url": "/React-demo_website/static/css/main.f6cde5d9.chunk.css"
+  },
+  {
+    "revision": "1494e039d68cad1e1dfa",
+    "url": "/React-demo_website/static/js/2.6d5e296e.chunk.js"
+  },
+  {
+    "revision": "8390064e73dd8ad4676060139e575712",
+    "url": "/React-demo_website/static/js/2.6d5e296e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7c7c76a98f3ce1bd8b",
+    "url": "/React-demo_website/static/js/main.d28a484e.chunk.js"
+  },
+  {
+    "revision": "b937fe51821c98862465",
+    "url": "/React-demo_website/static/js/runtime-main.995da652.js"
+  },
+  {
+    "revision": "62754a97bd86f1f7d5869a8fde33471a",
+    "url": "/React-demo_website/static/media/about_page.62754a97.svg"
+  },
+  {
+    "revision": "f14aa3e8a5cfda48323049423da6626f",
+    "url": "/React-demo_website/static/media/code_image.f14aa3e8.svg"
+  }
+]);
