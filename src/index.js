b4b9e243b796e5a186432652e93d7fca201b8285
lunnerwolf <abhigaur97@gmail.com>
2020-09-14 23:50:59 +0530

    my first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   
-  <BrowserRouter>
+  <BrowserRouter basename="/" >
     
     <App />
   
@@ -15,4 +15,4 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
-serviceWorker.unregister();
+
